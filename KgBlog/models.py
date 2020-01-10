4fefa8963b01d9ad22817418e0291c78da927cef
grim-GO <56546892+grim-GO@users.noreply.github.com>
2020-01-10 17:28:10 +0600

    commit 0.0.1

---
+++ KgBlog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+
+class Post(models.Model):
+    post_title = models.CharField("название поста", max_length=200)
+    post_text = models.TextField("текст поста")
+    pub_date = models.DateTimeField("дата публикации")
+
+    def __str__(self):
+        return self.post_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+
+class User(models.Model):
+    user_name = models.CharField("имя пользователя", max_length=50)
+    quantity_post = models.CharField("количество постов", max_length=20)
+    register_date = models.DateTimeField("дата регистрации")
+
+
+class Comment(models.Model):
+    author_name = models.CharField("имя пользователя", max_length=50)
+    comment_text = models.CharField("название поста", max_length=200)
+    comment_date = models.DateTimeField("дата комментирования")
+    post_comm = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author_name
