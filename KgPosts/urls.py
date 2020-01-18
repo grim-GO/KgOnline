e4fe9ab213683a4cbecd662f2ed578b90693a165
grim-GO <56546892+grim-GO@users.noreply.github.com>
2020-01-18 12:14:03 +0600

    commit0.2.0

--- KgPosts/urls.py
+++ KgPosts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
 
     url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-pub_date")[:100],
     template_name="posts.html")),
-
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Post, template_name="post.html"))
 ]
